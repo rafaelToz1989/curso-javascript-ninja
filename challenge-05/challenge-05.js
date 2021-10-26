cc7c9b6336b0158edd8765a976cf674fabb752f0
Rafael Tozi <rafa.tozi@hotmail.com>
2021-10-26 19:16:04 -0300

    challenge-05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var randomArray = ['Rafael', 32, true, 1.69, 'Quero ser ninja!', '85kg', null, undefined];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function returnArray(arrayParam){
+    return arrayParam;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(returnArray(randomArray)[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,24 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function mixedTypeValues(array, number){
+    return array[number];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var arrayList = [1, 'array item', true, null, {passe: 'verde', atencao: 'amarelo', pare: 'vermelho'}]
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+console.log(mixedTypeValues(arrayList, 0));
+console.log(mixedTypeValues(arrayList, 1));
+console.log(mixedTypeValues(arrayList, 2));
+console.log(mixedTypeValues(arrayList, 3));
+console.log(mixedTypeValues(arrayList, 4));
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +55,51 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(bookName) {
+
+    var bookList = {
+        'Clean Code': {
+            quantidadePaginas: 456,
+            autor: 'Robert C. Martin',
+            editora: 'Alta Books'
+        },
+        'Dracula': {
+            quantidadePaginas: 472,
+            autor: 'Bram Stoker',
+            editora: 'Nova Fronteira'
+        },
+        'Lovecraft Country': {
+            quantidadePaginas: 352,
+            autor: 'Matt Ruff',
+            editora: 'Intrinseca'
+        }
+    }
+
+    return !bookName ? bookList : bookList[bookName];
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log('O livro "Dracula" tem ' + book('Dracula').quantidadePaginas + ' páginas!');
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log('O autor do livro "Clean Code" é ' + book('Clean Code').autor + '.');
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log('O livro "Lovecraft Country" foi publicado pela editora ' + book('Lovecraft Country').editora + '.');
